4186129e8d17dcb84e22c07a9cdd146ed7c3ed26
anyanfei <87066062@qq.com>
2024-03-07 18:45:05 +0800

    提交一部分代码，确实写不完了

--- liteLoadQQNT_test.go
+++ liteLoadQQNT_test.go
@@ -1,11 +1,49 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestDownloadFile(t *testing.T) {
 	hasFileBool := downloadFile("https://github.com/Mzdyl/LiteLoaderQQNT_Install/releases/download/1.9/install_windows.exe", "windows_install")
 	log.Println(hasFileBool)
+}
+
+func TestGetQQPathInfo(t *testing.T) {
+	log.Println(getQQExePath())
+}
+
+func TestIsFileExists(t *testing.T) {
+	fmt.Println(isFileExists(filepath.Join(`d:\Program Files\Tencent\QQNT`, "dbghelp.dll")))
+	fmt.Println(isFileExists(`d:\Program Files\Tencent\QQNT`))
+}
+
+func TestGetFileMd5(t *testing.T) {
+	//5c4457f8ed767669d163c7376dbb97c9
+	log.Println(getFileMD5(filepath.Join(`d:\Program Files\Tencent\QQNT`, "QQ.exe")))
+}
+
+func getFileMD5(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
+
+func TestDownLoadAndInstallLiteLoader(t *testing.T) {
+	downLoadAndInstallLiteLoader(`d:\Program Files\Tencent\QQNT`)
 }
--- mian.go
+++ mian.go
@@ -1,24 +1,45 @@
 package main
 
 import (
+	"archive/zip"
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
+	"syscall"
 	"time"
 
 	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/registry"
 )
 
 const (
-	thisAppVersion = "1.0" // 当前版本号
+	thisAppVersion = "1.1"                         // 当前版本号，记得打git tag的时候这里要一致
+	proxyUrl       = "https://mirror.ghproxy.com/" // 存储反代服务器的URL
 
-	proxyUrl = "https://mirror.ghproxy.com/" // 存储反代服务器的URL
 )
 
+var (
+	SIG_X64 = []byte{0x48, 0x89, 0xCE, 0x48, 0x8B, 0x11, 0x4C, 0x8B, 0x41, 0x08, 0x49, 0x29, 0xD0, 0x48, 0x8B, 0x49, 0x18, 0xE8}
+	FIX_X64 = []byte{0x48, 0x89, 0xCE, 0x48, 0x8B, 0x11, 0x4C, 0x8B, 0x41, 0x08, 0x49, 0x29, 0xD0, 0x48, 0x8B, 0x49, 0x18, 0xB8, 0x01, 0x00, 0x00, 0x00}
+
+	SIG_X86 = []byte{0x89, 0xCE, 0x8B, 0x01, 0x8B, 0x49, 0x04, 0x29, 0xC1, 0x51, 0x50, 0xFF, 0x76, 0x0C, 0xE8}
+	FIX_X86 = []byte{0x89, 0xCE, 0x8B, 0x01, 0x8B, 0x49, 0x04, 0x29, 0xC1, 0x51, 0x50, 0xFF, 0x76, 0x0C, 0xB8, 0x01, 0x00, 0x00, 0x00}
+)
+
+type githubApiInfo struct {
+	Body    string `json:"body"`
+	TagName string `json:"tag_name"`
+}
+
+// go build -ldflags "-s -w" .
 func main() {
 	defer exitWithEnter()
 	// 检测是否以管理员模式运行
@@ -31,18 +52,40 @@ func main() {
 		log.Println("当前不是以管理员身份运行的，请关闭本程序并右击使用管理员身份运行，避免造成权限不足")
 		return
 	}
-	if !checkForUpdate() {
-		log.Println("检测失败")
+	qqExeFilePath, qqNTPath := getQQExePath()
+	if qqExeFilePath == "" || qqNTPath == "" {
+		log.Println("未找到QQ.exe绝对路径，请正确安装QQNT")
 		return
 	}
+	if !checkForUpdate() {
+		return
+	}
+	if isFileExists(filepath.Join(qqNTPath, "dbghelp.dll")) {
+		log.Println("检测到dbghelp.dll，推测你已修补QQ，跳过修补")
+	} else {
+		// 开始修补
+		if !patchPEFile(qqExeFilePath) {
+			log.Println("修复失败")
+			return
+		}
+	}
+	downLoadAndInstallLiteLoader(qqNTPath)
 
+}
+
+// 判断文件是否存在
+func isFileExists(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
 
 // 查看是否有新版本
 func checkForUpdate() bool {
-	resp, err := getHttpClient(3).Get("https://api.github.com/repos/anyanfei/go-liteLoaderQQNT/releases/latest")
+	resp, err := getHttpClient(3).Get("https://api.github.com/repos/anyanfei/go-liteLoadQQNT/releases/latest")
 	if err != nil {
-		log.Println("检查更新时发生错误，无法获取https://api.github.com/repos/anyanfei/go-liteLoaderQQNT/releases/latest信息")
+		log.Println("检查更新时发生错误，无法获取https://api.github.com/repos/anyanfei/go-liteLoadQQNT/releases/latest信息")
 		return false
 	}
 	defer resp.Body.Close()
@@ -51,48 +94,64 @@ func checkForUpdate() bool {
 		log.Println(err)
 		return false
 	}
-	fmt.Println(string(respByte))
-	return true
-}
-
-//def check_for_updates():
-//try:
-//# 获取最新版本号
-//response = requests.get("https://api.github.com/repos/Mzdyl/LiteLoaderQQNT_Install/releases/latest", timeout=3)
-//latest_release = response.json()
-//tag_name = latest_release['tag_name']
-//body = latest_release['body']
-//if tag_name > current_version:
-//print(f"发现新版本 {tag_name}！")
-//print(f"更新日志：\n ")
-//console = Console()
-//markdown = Markdown(body)
-//console.print(markdown)
-//download_url = (
-//f"https://github.com/Mzdyl/LiteLoaderQQNT_Install/releases/download/{tag_name}/install_windows.exe")
-//# urllib.request.urlretrieve(download_url, f"install_windows-{tag_name}.exe")
-//download_file(download_url, f"install_windows-{tag_name}.exe", PROXY_URL)
-//print("版本号已更新。")
-//print("请重新运行脚本。")
-//sys.exit(0)
-//else:
-//print("当前已是最新版本，开始安装。")
-//except Exception as e:
-//print(f"检查更新阶段发生错误: {e}")
-
-// 是否可以连接代理的github，能则返回true，反之false
-func canConnectToGithubWithProxy() bool {
-	resp, err := getHttpClient(5).Get(proxyUrl + "https://github.com/Mzdyl/LiteLoaderQQNT_Install/releases/download/1.9/install_windows.exe")
+	var tempGithubApiInfo githubApiInfo
+	err = json.Unmarshal(respByte, &tempGithubApiInfo)
 	if err != nil {
 		log.Println(err)
 		return false
 	}
-	log.Println(resp.StatusCode)
+	latestTagName := tempGithubApiInfo.TagName
+	latestBody := tempGithubApiInfo.Body
+	if latestTagName > thisAppVersion { // 发现新版本，下载新版本
+		log.Println("发现新版本", latestTagName)
+		log.Println("更新日志：", latestBody)
+		newFileName := fmt.Sprintf("liteLoadQQNT-%s", latestTagName)
+		log.Println("正在下载新版本，这可能需要一些时间")
+		downloadBool := downloadFile("https://github.com/anyanfei/go-liteLoadQQNT/releases/download/"+latestTagName+"/liteLoadQQNT.exe", newFileName)
+		if !downloadBool {
+			return false
+		}
+		log.Println("版本号已更新，请重新运行执行新文件", newFileName+".exe")
+		return false
+	}
+	log.Println("当前已经是最新版本，开始安装")
+	return true
+}
+
+// 返回QQNT exe的绝对路径和当前QQNT的路径绝对
+func getQQExePath() (string, string) {
+	keyInfo, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\QQ`, registry.READ)
+	if err != nil {
+		log.Println(err)
+		return "", ""
+	}
+	defer keyInfo.Close()
+	uninstallStringValue, _, err := keyInfo.GetStringValue("UninstallString")
+	if err != nil {
+		log.Println(err)
+		return "", ""
+	}
+	if uninstallStringValue == "" {
+		log.Println("未找到QQNT的安装目录，请加QQ群：1048452984截图咨询")
+		return "", ""
+	}
+	qqExePath := strings.Replace(uninstallStringValue, "Uninstall.exe", "QQ.exe", 1)
+	return qqExePath, filepath.Dir(qqExePath)
+}
+
+// 是否可以连接代理的github，能则返回true，反之false
+func canConnectToGithubWithProxy(downloadUrl string) bool {
+	resp, err := getHttpClient(5).Get(proxyUrl + downloadUrl)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
 	return resp.StatusCode == http.StatusOK
 }
 
+// 下载文件，并重命名文件名字(fileName只需要传名字不用包含后缀)
 func downloadFile(needDownloadUrl, fileName string) bool {
-	if canConnectToGithubWithProxy() {
+	if canConnectToGithubWithProxy(needDownloadUrl) {
 		needDownloadUrl = proxyUrl + needDownloadUrl
 	}
 	res, err := getHttpClient(60).Get(needDownloadUrl)
@@ -114,6 +173,165 @@ func downloadFile(needDownloadUrl, fileName string) bool {
 		return false
 	}
 	fmt.Printf("下载文件成功，文件长度为 :%d\r\n", written)
+	return true
+}
+
+func scanAndReplace(buffer *[]byte, pattern []byte, replacement []byte) {
+	index := bytes.Index(*buffer, pattern)
+	for index != -1 {
+		*buffer = append((*buffer)[:index], append(replacement, (*buffer)[index+len(pattern):]...)...)
+		fmt.Printf("Found at 0x%X", index)
+		index += len(replacement)
+		index = bytes.Index((*buffer)[index:], pattern)
+	}
+}
+
+// 修补exe文件
+func patchPEFile(filePath string) bool {
+	savePath := filePath + ".bak"
+	err := os.Rename(filePath, savePath)
+	if err != nil {
+		log.Println("Error renaming file:", err)
+		return false
+	}
+	log.Printf("已将原版备份在 : %s\r\n", savePath)
+
+	peFileBytes, err := os.ReadFile(savePath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return false
+	}
+	if 32<<(^uint(0)>>63) == 32 { // 判断当前是64位还是32位系统
+		scanAndReplace(&peFileBytes, SIG_X86, FIX_X86)
+	} else {
+		scanAndReplace(&peFileBytes, SIG_X64, FIX_X64)
+	}
+	err = os.WriteFile(filePath, peFileBytes, 0644)
+	if err != nil {
+		log.Println("Error writing file:", err)
+		return false
+	}
+	log.Println("修补成功！")
+	return true
+}
+
+func downLoadAndInstallLiteLoader(qqNTPath string) bool {
+	log.Println("正在拉取LiteLoaderQQNT最新版本的仓库...")
+	//downloadBool := downloadFile(`https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/archive/master.zip`, "LiteLoader")
+	//if !downloadBool {
+	//	return false
+	//}
+	thisDir, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	log.Println("开始解压到LiteLoader文件夹")
+	unZip(filepath.Join(thisDir, "LiteLoader.zip"), "LiteLoader")
+	log.Println("拉取完成，正在安装 LiteLoaderQQNT")
+	// 移除目标路径及其内容
+	LiteLoaderQQNTBakPath := filepath.Join(qqNTPath, `resources`, `app`, `LiteLoaderQQNT_bak`)
+	if isFileExists(LiteLoaderQQNTBakPath) {
+		err = os.RemoveAll(LiteLoaderQQNTBakPath)
+		if err != nil {
+			log.Println("请完全关闭QQ，重新执行")
+			return false
+		}
+	}
+	sourceDir := filepath.Join(qqNTPath, `resources`, `app`, `LiteLoaderQQNT-main`)
+	destinationDir := filepath.Join(LiteLoaderQQNTBakPath)
+	log.Println("从", filepath.Join(thisDir, `LiteLoader`, `LiteLoaderQQNT-main`), "移动到", sourceDir)
+	if isFileExists(sourceDir) {
+		err = os.Rename(sourceDir, destinationDir)
+		if err != nil {
+			log.Println("请完全关闭QQ，重新执行", err)
+			return false
+		}
+		log.Println("已将旧版重命名为: ", destinationDir)
+	} else {
+		log.Println(sourceDir, "不存在，全新安装")
+		err = moveFolder(filepath.Join(thisDir, `LiteLoader`, `LiteLoaderQQNT-main`), sourceDir)
+		if err != nil {
+			log.Println("移动文件夹出错", err)
+			return false
+		}
+	}
+	return true
+}
+
+// 移动文件夹
+func moveFolder(src, dst string) error {
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(src, path)
+		if err != nil {
+			return err
+		}
+		dstPath := filepath.Join(dst, relPath)
+		if info.IsDir() {
+			return os.MkdirAll(dstPath, info.Mode())
+		} else {
+			return moveOneFile(path, dstPath)
+		}
+	})
+}
+
+// 跨卷移动
+func moveOneFile(oldPath, newPath string) error {
+	from, err := syscall.UTF16PtrFromString(oldPath)
+	if err != nil {
+		return err
+	}
+	to, err := syscall.UTF16PtrFromString(newPath)
+	if err != nil {
+		return err
+	}
+	return syscall.MoveFile(from, to)
+}
+
+// 解压一个zip包，传入zip包的绝对地址和需要输出的当前路径的文件
+func unZip(zipPath string, outPutFileName string) bool {
+	archive, err := zip.OpenReader(zipPath)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer archive.Close()
+	for _, f := range archive.File {
+		filePath := filepath.Join(outPutFileName, f.Name)
+		fmt.Println("开始解压文件... ", filePath)
+		if !strings.HasPrefix(filePath, filepath.Clean(outPutFileName)+string(os.PathSeparator)) {
+			log.Println("无效的文件路径")
+			return false
+		}
+		if f.FileInfo().IsDir() {
+			fmt.Println("创建目录...")
+			os.MkdirAll(filePath, os.ModePerm)
+			continue
+		}
+		if err = os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+			log.Println(err)
+			return false
+		}
+		dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			log.Println(err)
+			return false
+		}
+		fileInArchive, err := f.Open()
+		if err != nil {
+			log.Println(err)
+			return false
+		}
+		if _, err = io.Copy(dstFile, fileInArchive); err != nil {
+			log.Println(err)
+			return false
+		}
+		dstFile.Close()
+		fileInArchive.Close()
+	}
 	return true
 }
 
